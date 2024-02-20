6726359d448181a8cf5c1b91b85b2e97485bd928
Rogério Lacerda <nenorogerinho@gmail.com>
2024-02-20 14:09:34 -0300

    Adicionando Conteúdo da pagina de login

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,9 +1,13 @@
+import Footer from '../../components/Footer'
 import Header from '../../components/Header'
+import LoginContent from '../../components/LoginContent'
 
 const Login = () => {
   return (
     <div>
       <Header/>
+      <LoginContent/>
+      <Footer/>
     </div>
   )
 }
