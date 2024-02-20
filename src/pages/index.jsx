9bd46176e20953bf26fc19d72c2b34a82d54f480
Rogério Lacerda <nenorogerinho@gmail.com>
2024-02-19 23:49:50 -0300

    Adcionando lading page e componente header

---
+++ src/pages/index.jsx
@@ -0,0 +1,73 @@
+import styles from './styles.module.css'
+import {Link} from 'react-router-dom';
+import sete from '../assets/img-700x700.jpg';
+import quatro from '../assets/img-450x450.png';
+import Header from '../components/Header';
+
+const LadingPage = () => {
+  return (
+    <>
+      <div className={styles.page}>
+        <div className={styles.every}>
+          <Header/>
+          <section className={styles.firstSection}>
+
+            <div className={styles.container}>
+              <div className={styles.infoContainer}>
+                <h1>SHAYPADO</h1>
+                <p>Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                  <Link to="/home">baixe o app</Link>
+                  <Link to="/login">login</Link>
+              </div>
+              <div className={styles.imgDiv}>
+                <img src={sete} alt="img"/>
+              </div>
+            </div>
+
+          </section>
+          <section className={styles.secondSection}>
+
+            <div className={styles.container}>
+              <img src={quatro} alt="foto 1"/>
+              <img src={quatro} alt="foto 2"/>
+              <img src={quatro} alt="foto 3"/>
+            </div>
+
+            <div className={styles.container}>
+              <div>
+                <img src={sete} alt="fotão"/>
+              </div>
+              <div>
+                <p>
+                  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+                </p>
+              </div>
+            </div>
+
+            <div className={styles.container}>
+              <div>
+                <img src={sete} alt="fotão"/>
+              </div>
+              <div>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+              </div>
+            </div>
+
+            <div className={styles.container}>
+              <a href="">
+                baixe o app
+              </a>
+            </div>
+
+          </section>
+
+          <footer>
+            algumas infos do roda pé
+          </footer>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default LadingPage
