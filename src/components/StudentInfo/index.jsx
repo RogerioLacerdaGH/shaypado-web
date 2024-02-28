564af13d120b15c41c1055a117d3ffb16dcecac7
Rogério Lacerda <nenorogerinho@gmail.com>
2024-02-27 22:04:00 -0300

    adicionando componente da página de editar aluno

---
+++ src/components/StudentInfo/index.jsx
@@ -0,0 +1,28 @@
+import styles from './styles.module.css'
+
+const StudentInfo = () => {
+  return (
+    <div>
+      <button>edit</button>
+      <button>historico</button>
+      <div>
+        <div>
+          <img src="" alt="foto do cabra" />
+        </div>
+        <div>
+          <p>nome do aluno:</p>
+          <p>Email/Contato</p>
+        </div>
+      </div>
+      <div>
+        <p>informações gerais:</p>
+        <p>altura; idade peso etc etc</p>
+      </div>
+      <div>
+       <h3>treino:</h3>
+      </div>
+    </div>
+  )
+}
+
+export default StudentInfo
