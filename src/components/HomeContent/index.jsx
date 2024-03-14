2f212888859a1fd00191672c5ff8517a28a0a549
Rogério Lacerda <nenorogerinho@gmail.com>
2024-03-14 15:44:44 -0300

    pequenos ajustes visuais

--- src/components/HomeContent/index.jsx
+++ src/components/HomeContent/index.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
 import styles from './styles.module.css'
-import Header from '../Header'
 import { Link } from 'react-router-dom'
 import { Container } from 'react-bootstrap'
 
